ee50f47e41eb9e8f781707db02abb0c6aefb2207
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-10-01 19:08:17 +0100

    fix error's pycodestyle
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- Module00/ex01/exec.py
+++ Module00/ex01/exec.py
@@ -1,8 +1,9 @@
 import sys
 
 if (len(sys.argv) > 1):
-  list = sys.argv[::-1][:-1]
-  for str in list:
-    print(str[::-1].swapcase(), end=' ' if str != list[-1] else '\n')
+    list = sys.argv[::-1][:-1]
+    for str in list:
+        print(str[::-1].swapcase(), end=' ' if str != list[-1] else '\n')
 
-else: print('Please enter at least one argument')
+else:
+    print('Please enter at least one argument')
