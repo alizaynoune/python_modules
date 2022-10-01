5d74a1b2e787b02aa5de501a37543cf34133d173
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-10-01 18:59:58 +0100

    Module00/ex01 done
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

---
+++ Module00/ex01/exec.py
@@ -0,0 +1,8 @@
+import sys
+
+if (len(sys.argv) > 1):
+  list = sys.argv[::-1][:-1]
+  for str in list:
+    print(str[::-1].swapcase(), end=' ' if str != list[-1] else '\n')
+
+else: print('Please enter at least one argument')
