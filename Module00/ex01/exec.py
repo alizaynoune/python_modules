ff3d0cc7055d3a51e476c3aaa39f74cd48ae415d
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-10-02 09:55:56 +0100

    Module00/ex01 update
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

--- Module00/ex01/exec.py
+++ Module00/ex01/exec.py
@@ -1,9 +1,7 @@
 import sys
 
 if (len(sys.argv) > 1):
-    list = sys.argv[::-1][:-1]
-    for str in list:
-        print(str[::-1].swapcase(), end=' ' if str != list[-1] else '\n')
-
+    str = " ".join([i.swapcase()[::-1] for i in sys.argv[::-1][:-1]])
+    print(str)
 else:
     print('Please enter at least one argument')
