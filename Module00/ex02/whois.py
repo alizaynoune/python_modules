25684af9a5cdae35a8e1fd030409d32e4351204c
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-10-02 11:39:09 +0100

    Module00/ex02 done
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

---
+++ Module00/ex02/whois.py
@@ -0,0 +1,20 @@
+import sys
+
+len = len(sys.argv) - 1
+if (len == 1):
+    try:
+        number = int(sys.argv[1])
+        if not number:
+            print('I\'m Zero.')
+        else:
+            message = 'Even' if not number % 2 else 'Odd'
+            print('I\'m {0}.'.format(message))
+    except ValueError:
+        sys.exit('AssertionError: argument is not an integer')
+    except ValueError as e:
+        sys.exit('AssertionError: {0}.'.format(e))
+
+elif not (len):
+    sys.exit('Usage: python whois [number]')
+elif not (len == 1):
+    sys.exit('AssertionError: more than one argument are provided')
