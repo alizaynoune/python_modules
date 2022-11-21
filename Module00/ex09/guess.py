987e318b64215497d64e2a5ac828ca13a393889e
Ali Zaynoune <zaynoune.ali@gmail.com>
2022-11-21 10:57:01 +0100

    Module00/ex09 done
    Signed-off-by: Ali Zaynoune <zaynoune.ali@gmail.com>

---
+++ Module00/ex09/guess.py
@@ -0,0 +1,32 @@
+import random
+secret = random.randint(1, 99)
+
+print("This is an interactive guessing game!")
+print("You have to enter a number between 1 and 99\
+to find out the secret number.")
+print("Type 'exit' to end the game.\nGood luck!\n")
+i = 1
+while True:
+    guess = input("What's your guess between 1 and 99?\n")
+    if guess == 'exit':
+        print("Goodbye!")
+        break
+    if not guess.isnumeric():
+        print("That's not a number.")
+    else:
+        if int(guess) == secret:
+            if i == 1:
+                print(
+                    "The answer to the ultimate question of life,\
+the universe and everything is %d." % secret)
+                print("Congratulations! You got it on your first try!")
+                break
+            else:
+                print("Congratulations, you've got it!\
+You won in %d attempts!" % i)
+                break
+        elif int(guess) < secret:
+            print("Too low!")
+        elif int(guess) > secret:
+            print("Too high!")
+    i += 1
