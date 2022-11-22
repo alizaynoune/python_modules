8222a08ee356af59134bf51927b6ebfb0520357b
alizaynoune <zaynoune.ali@gmail.com>
2022-11-22 23:00:51 +0100

    module01/ex01 init
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- Module01/ex00/book.py
+++ Module01/ex00/book.py
@@ -1,10 +1,32 @@
-from typing import Literal
+
+def validation(name, last_update, creation_date, recipes_list) -> None:
+    if not name or not isinstance(name, str):
+        raise ValueError('The name must be string')
+    elif not recipes_list or not isinstance(recipes_list, dict) or not recipes_list in ["starter", "lunch", "dessert"]:
+        raise ValueError(
+            'The recipes_list must can be "starter", "lunch" or "dessert"')
 
 
 class Book:
-    def __init__(self, name: str, last_update, creation_date, recipes_list: Literal["starter", "lunch", "dessert"]) -> None:
+    def __init__(self, name, last_update, creation_date, recipes_list) -> None:
+        try:
+            validation(name, last_update, creation_date, recipes_list)
+        except Exception as e:
+            print(e)
         pass
     # • recipes_list (dict): a dictionnary with 3 keys: "starter", "lunch", "dessert".
 
     def __str__(self) -> str:
         pass
+
+    def get_recipe_by_name(self, name):
+        """Prints a recipe with the name \texttt{name} and returns the instance"""
+        # ... Your code here ...
+
+    def get_recipes_by_types(self, recipe_type):
+        """Get all recipe names for a given recipe_type """
+        # ... Your code here ...
+
+    def add_recipe(self, recipe):
+        """Add a recipe to the book and update last_update"""
+        # ... Your code here ...
--- Module01/ex00/recipe.py
+++ Module01/ex00/recipe.py
@@ -1,10 +1,32 @@
-from typing import Literal
+def validation(name, cooking_lvl, cooking_time, ingredients, description, recipe_type) -> None:
+    if not name or not isinstance(name, str):
+        raise ValueError('The name must be string')
+    elif not isinstance(cooking_lvl, int) or not 1 <= cooking_lvl <= 5:
+        raise ValueError('The cooking_lvl must be integer between 1, 5')
+    elif not isinstance(cooking_time, int) or 0 > cooking_time:
+        raise ValueError('The cooking_time must be a positive number')
+    elif not ingredients or not all(isinstance(v, str) for v in ingredients):
+        raise ValueError(
+            'The ingredients must be list of all ingredients each represented by a string')
+    elif description and not isinstance(description, str):
+        raise ValueError('The description must be string')
+    elif not recipe_type or not isinstance(recipe_type, str) or not recipe_type in ["starter", "lunch", "dessert"]:
+        raise ValueError(
+            'The recipe_type must can be "starter", "lunch" or "dessert"')
 
 
 class Recipe:
-    def __init__(self, name: str, cooking_lvl: int, cooking_time: int, ingredients: list, description: str, recipe_type: Literal["starter", "lunch", "dessert"]) -> None:
+    def __init__(self, name, cooking_lvl, cooking_time, ingredients, description, recipe_type) -> None:
+        try:
+            validation(name, cooking_lvl, cooking_time,
+                       ingredients, description, recipe_type)
+        except Exception as e:
+            print(e)
         pass
         # recipe_type (str): can be "starter", "lunch" or "dessert".
 
     def __str__(self) -> str:
         pass
+
+
+Recipe('test', 1, 2, ['test'], None, 'dessert')
