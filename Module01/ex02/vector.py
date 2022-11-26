aa284335c260568857826017324a1e1ee8426737
alizaynoune <zaynoune.ali@gmail.com>
2022-11-26 21:39:01 +0100

    module01/ex02 done
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- Module01/ex02/vector.py
+++ Module01/ex02/vector.py
@@ -1,8 +1,6 @@
 class Vector:
     def __init__(self, values) -> None:
         self.values = [[]]
-        # a list of a list of floats: Vector([[0.0, 1.0, 2.0, 3.0]]),
-        # a list of lists of single float: Vector([[0.0], [1.0], [2.0], [3.0]]),
         if isinstance(values, list):
             val_len = len(values)
             if val_len == 1 and not len(values[0]):
@@ -13,87 +11,91 @@ class Vector:
                         raise ValueError('error {}'.format(i))
                 else:
                     if not isinstance(i, float):
-                        raise ValueError('error {}'.format(i))
+                        raise ValueError('error2 {}'.format(i))
 
             self.values = values[:]
             self.shape = (val_len, len(values[0]))
-        # Vector(3) -> the vector will have values = [[0.0], [1.0], [2.0]],
         elif isinstance(values, int):
             if values <= 0:
                 print('error')
-            # [c for c in new_list if len(c) > minLen]
-            self.shape = (1, values)
-            self.values = [[float(i) for i in range(values)]]
-        elif isinstance(values, range):
-            if values.start < 0 or values.start >= values.stop:
+            self.shape = (values, 1)
+            self.values = [[float(i)] for i in range(values)]
+        elif isinstance(values, tuple):
+            if len(values) != 2 or values[0] < 0 or values[0] >= values[1]:
                 raise ValueError('error {}'.format(values))
-            self.shape = (1, values.stop - values.start)
-            self.values = [[float(i) for i in values]]
+            self.shape = (values[1] - values[0], 1)
+            self.values = [[float(i)] for i in values]
 
         pass
 
-    def __add__(self, value):
-        if self.shape[0] >= self.shape[1]:
-            self.values = [[j + value] for i in self.values for j in i]
-        else:
-            self.values = [[j + value for i in self.values for j in i]]
-        return self
+    def __add__(self, vec):
+        if not isinstance(vec, Vector) or self.shape != vec.shape:
+            raise ValueError("Vectors must have the same dimensions.")
 
-    def __radd__(self, v):
-        return self.__add__(v)
+        if vec.shape[0] >= vec.shape[1]:
+            return Vector([[a[0] + b[0]] for (a, b) in zip(self.values, vec.values)])
+        else:
+            return Vector([[a[0] + b[0] for (a, b) in zip(self.values, vec.values)]])
+
+    def __radd__(self, vec):
+        return self.__add__(vec)
     # add & radd : only vectors of same shape.
 
-    def __sub__(self):
-        print('sub done')
+    def __sub__(self, vec):
+        if not isinstance(vec, Vector) or self.shape != vec.shape:
+            raise ValueError("Vectors must have the same dimensions.")
+        if vec.shape[0] >= vec.shape[1]:
+            return Vector([[a[0] * b[0]] for (a, b) in zip(self.values, vec.values)])
+        else:
+            return Vector([[a[0] * b[0] for (a, b) in zip(self.values, vec.values)]])
 
-    def __rsub__(self):
-        print('rsub done')
+    def __rsub__(self, vec):
+        return self.__sub__(vec)
     # sub & rsub: only vectors of same shape.
 
-    def __truediv__(self, value):
-        if value == 0:
-            raise ValueError('ZeroDivisionError: division by zero.')
-        if self.shape[0] >= self.shape[1]:
-            self.values = [[j / value] for i in self.values for j in i]
+    def __truediv__(self, scalar):
+        if not isinstance(scalar, int) and not isinstance(scalar, float):
+            raise ValueError('Scalar must be int or float')
+        if scalar == 0:
+            raise ZeroDivisionError('division by zero.')
+        res = Vector(self.values)
+        if res.shape[0] >= res.shape[1]:
+            res.values = [[j / scalar] for i in res.values for j in i]
         else:
-            self.values = [[j / value for i in self.values for j in i]]
-        return self
+            res.values = [[j / scalar for i in res.values for j in i]]
+        return res
     # truediv : only with scalars (to perform division of Vector by a scalar).
 
     def __rtruediv__(self, value):
-        raise ValueError(
-            'this test can be commented for the other tests and uncommented to show this one')
+        raise NotImplementedError(
+            'Division of a scalar by a Vector is not defined here.')
     # rtruediv : raises an NotImplementedError with the message "Division of a scalar by a Vector is not defined here."
 
-    def __mul__(self, value):
-        if self.shape[0] >= self.shape[1]:
-            self.values = [[j * value] for i in self.values for j in i]
+    def __mul__(self, scalar):
+        if not isinstance(scalar, int) and not isinstance(scalar, float):
+            raise ValueError('Scalar must be int or float')
+        res = Vector(self.values)
+        if res.shape[0] >= res.shape[1]:
+            res.values = [[j * scalar] for i in res.values for j in i]
         else:
-            self.values = [[j * value for i in self.values for j in i]]
+            res.values = [[j * scalar for i in res.values for j in i]]
 
-        return self
+        return res
 
-    def __rmul__(self, v):
-        return self.__mul__(v)
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
     # mul & rmul: only scalars (to perform multiplication of Vector by a scalar).
 
     def __str__(self):
         return f'Vector({self.values})'
 
     def __repr__(self):
-        print('repr done')
+        return f'<Vector of size {self.shape} {self.values}>'
     # must be identical, i.e we expect that print(vector) and vector within python interpretor behave the same, see correspond
 
     def dot(self, value):
-        #  res = 0
         if not isinstance(value, Vector) or self.shape != value.shape:
             raise ValueError("Vectors must have the same dimensions.")
-        # for i, val in enumerate(self.values):
-        #     if isinstance(val, list):
-        #         res += val[0] * v.values[i][0]
-        #     else:
-        #         res += val * v.values[i]
-        # return res
         res = 0
         for i in range(len(self.values)):
             for j in range(len(self.values[i])):
@@ -102,11 +104,7 @@ class Vector:
         return res
 
     def T(self):
-        if self.shape == (1, 1):
-            return self
         if self.shape[0] >= self.shape[1]:
-            self.values = [[j for i in self.values for j in i]]
+            return Vector([[j for i in self.values for j in i]])
         else:
-            self.values = [[j] for i in self.values for j in i]
-        self.shape = (len(self.values), len(self.values[0]))
-        return self
+            return Vector([[j] for i in self.values for j in i])
