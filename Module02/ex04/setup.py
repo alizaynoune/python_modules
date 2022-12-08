0aef5c513331586e7a275f1952a8122e6f03eb5d
alizaynoune <zaynoune.ali@gmail.com>
2022-12-08 19:01:43 +0100

    module02/ex04 done
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

--- Module02/ex04/setup.py
+++ Module02/ex04/setup.py
@@ -6,20 +6,7 @@ setuptools.setup(
     author='Ali Zaynoune',
     author_email='alzaynou@student.1337.ma',
     url='https://github.com/alizaynoune/python_modules/blob/main/Module02/ex04',
-    # license='GPLv3',
     description='Howto create a package in python.',
     long_description=open('README.md').read(),
-    # packages=setuptools.find_packages(where="src"),
     python_requires='>=3.7',
-    # Classifiers=[
-    #     "Development Status :: 3 - Alpha",
-    #     "Intended Audience :: Developers",
-    #     "Intended Audience :: Students",
-    #     "Topic :: Education",
-    #     "Topic :: HowTo",
-    #     "Topic :: Package",
-    #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    #     "Programming Language :: Python :: 3",
-    #     "Programming Language :: Python :: 3 :: Only",
-    # ]
 )
