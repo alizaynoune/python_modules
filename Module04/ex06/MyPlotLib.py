0dc485d3fdd20d06c85c387c585c97a2474bc9c9
alizaynoune <zaynoune.ali@gmail.com>
2022-12-18 23:28:49 +0100

    module04/ex06 init
    Signed-off-by: alizaynoune <zaynoune.ali@gmail.com>

---
+++ Module04/ex06/MyPlotLib.py
@@ -0,0 +1,17 @@
+
+
+class MyPlotLib:
+    def __init__(self) -> None:
+        pass
+
+    def histogram(data, features):
+        pass
+
+    def density(data, features):
+        pass
+
+    def pair_plot(data, features):
+        pass
+
+    def box_plot(data, features):
+        pass
